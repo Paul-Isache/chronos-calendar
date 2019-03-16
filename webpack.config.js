b7ed0011ad80345b87f3e7e1056513ce1bd67c53
George Chetreanu <chetreanu.george@gmail.com>
2019-03-16 14:20:48 +0200

    refactored to use hooks & removed demo from this repo

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,43 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    plugins: [
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: devMode ? '[name].css' : 'Calendar.css',
-            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-        })
-    ],
-    mode: 'production',
-    entry: './src/Calendar.jsx',
-    output: {
-        path: path.resolve('build'),
-        filename: 'Calendar.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules)/,
-            use: 'babel-loader'
-        },
-        {
-            test: /\.css$/,
-            use: [
-                devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                'css-loader',
-            ]
-        }]
-    }
+  mode: 'production',
+  entry: './src/Calendar.jsx',
+  output: {
+    path: path.resolve('build'),
+    filename: 'Calendar.js',
+    library: "ChronosCalendar", // string,
+    libraryTarget: "umd", // universal module definition
+    //libraryTarget: 'commonjs2'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : 'Calendar.css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /.jsx$/,
+        include: [
+          path.resolve(__dirname, "src")
+        ],
+        exclude: [
+          path.resolve(__dirname, "node_modules")
+        ],
+        loader: "babel-loader"
+      },
+      {
+        test: /.css$/,
+        loader: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  }
 }
